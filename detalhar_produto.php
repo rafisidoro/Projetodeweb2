4014b2f3c8a518465dbb84467c6f39cbd26b4ec0
Maria Morais <75227121+moraisabella@users.noreply.github.com>
2021-01-10 22:29:25 -0300

    Add files via upload

---
+++ detalhar_produto.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0">
+    <title> Produtos cadastrados </title>
+<link rel="stylesheet" href="layout1.css"> 
+    <link href="https://fonts.googleapis.com/css?family=Cinzel&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php
+    require_once "conexao.php";
+    
+    $idProduto = $_GET['idProduto'];
+
+    $comando = "SELECT * FROM produto u INNER JOIN categoria e ON u.idCategoria = e.idCategoria WHERE u.idProduto = $idProduto";
+    $result = mysqli_query($conexao, $comando);
+    $comando2 = "SELECT * FROM produto WHERE idProduto = $idProduto";
+    $result2 = mysqli_query($conexao, $comando2);
+    $linha2= mysqli_fetch_assoc($result2);
+
+    while ($linha = mysqli_fetch_assoc($result)){
+?>
+    <p> Produto: <?=$linha['nomeproduto']?></p>
+    <p> Descrição: <?=$linha2['descricao']?></p>
+    <p> Preço: <?=$linha['preco']?></p>
+    <p> Quantidade em estoque: <?=$linha['quant_estoque']?></p>
+    <p> Categoria: <?=$linha['idCategoria']?></p>
+<?php
+    }
+    
+?>
+ <div id ="volt">
+            <a id="voltar" href="index.php" > Voltar a página inicial</a>
+            </div>
+            
+            <div id="rodape">
+                <img id="pag" src="pagamento.png" alt="pagamentos">
+                <img id="redes" src="redes.png" alt="redessociais">
+                <div id="linkr">
+                <table>
+                    <tr>
+                    <li> <a href="cadastroproduto2.html"> Página do administrador </a></li>
+                    </tr>
+                </table>
+                </div>
+                </div>       
+</body>
